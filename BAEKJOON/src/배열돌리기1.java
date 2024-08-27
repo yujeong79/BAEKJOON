7411c1ffc102e297e224a59a19bad70d0d9514c8
김유정 <aabbc0908@naver.com>
2024-08-27 14:10:15 +0900

    2024-08-27(14:10)

--- BAEKJOON/src/배열돌리기1.java
+++ BAEKJOON/src/배열돌리기1.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 배열돌리기1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 	}
 }
