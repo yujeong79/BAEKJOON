aa040c0f9cc95993d629364a6345728d3331420d
김유정 <aabbc0908@naver.com>
2024-08-29 08:54:07 +0900

    commit

--- BAEKJOON/src/Main_BAEKJOON_12904_A와B_Gold5.java
+++ BAEKJOON/src/Main_BAEKJOON_12904_A와B_Gold5.java
@@ -9,53 +9,34 @@ public class Main_BAEKJOON_12904_A와B_Gold5 {
 	static List<Character> sList;
 	
 	public static void main(String[] args) throws IOException {
-		String str = br.readLine();
-		sList = new ArrayList<>();
-		for(int i = 0; i < str.length(); i++) {
-			sList.add(str.charAt(i));
-		}
+		char[] S = br.readLine().toCharArray();
 		
 		char[] T = br.readLine().toCharArray();
+		List<Character> tList = new ArrayList<>();
+		for(int i = 0; i < T.length; i++) {
+			tList.add(T[i]);
+		}
 		
-		boolean flag = true;
+		while(tList.size() > S.length) {
+			int last = tList.size()-1;
+			if(tList.get(last) == 'A') {
+				tList.remove(last);
+			} else {
+				tList.remove(last);
+				Collections.reverse(tList);
+			}
+		}
 		
-		while(flag && sList.size() < T.length) { // sList가 T만큼의 문자열을 완성하면 종료
-			List<Character> tmp = sList;
-			
-			// 일단 A를 추가해봐
-			tmp.add('A');
-			for(int i = 0; flag && i < tmp.size(); i++) {
-				//System.out.println(tmp.get(i) + ", " + T[i]);
-				if(tmp.get(i) != T[i]) flag = false;
+		int flag = 1;
+		for(int i = 0; i < S.length; i++) {
+			if(tList.get(i) != S[i]) {
+				flag = 0;
+				break;
 			}
-			
-			// A 추가했는데 안됐어?
-			if(!flag) {
-				flag = true;
-				Collections.reverse(tmp); // 그러면 뒤집어봐
-				for(int i = 0; flag && i < tmp.size(); i++) {
-					if(tmp.get(i) != T[i]) flag = false;
-				}
-				if(flag) { // 뒤집었는데 같으면 B를 넣어야해
-					tmp.add('B');
-				}
-			}
-			
-			if(!flag) { // A를 추가하고 뒤집어서 	비교도 했는데 안됐어?
-				tmp.remove(tmp.size()-1); // 원래 추가했던 A를 일단 빼고
-				flag = true; // flag도 일단 true 해놔
-				
-				Collections.reverse(tmp);
-				tmp.add('B');
-				for(int i = 0; flag && i < tmp.size(); i++) {
-					//System.out.println(tmp.get(i) + ", " + T[i]);
-					if(tmp.get(i) != T[i]) flag = false;
-				}
-			}
-			
-			if(flag) sList = tmp;
-		} 
+		}
 		
-		System.out.println(flag ? 1 : 0);
+		System.out.println(flag);
+		
+		
 	} // end of main
 } // end of class
