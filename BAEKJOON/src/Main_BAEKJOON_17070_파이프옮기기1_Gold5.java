672be96413f71b0f51e5924ccf8c8fcafa83d04d
김유정 <aabbc0908@naver.com>
2024-09-03 08:53:13 +0900

    commit

--- BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
+++ BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
@@ -10,11 +10,6 @@ public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {
 	
 	private static int N;
 	private static int[][] map;
-	private static int[][] pipeDirection;
-	
-	private static int r;
-	private static int c;
-	private static int d; // 파이프의 방향 1 : 가로, 2 : 세로, 3 : 대각선 
 
 	private static int answer;
 	
@@ -30,27 +25,37 @@ public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {
 			}
 		}
 		
-		r = 0; c = 1; // 현재 파이프의 끝점
-		d = 1;
+		Move(0, 1, 1);
 		
-		pipeDirection = new int[N][N];
-		pipeDirection[r][c] = d;
-		Move();
-	}
-
-	private static void Move() {
-		if(r >= N-1 && c >= N-1) {
+		System.out.println(answer);
+	} // end of main
+	
+	/**
+	 * @param row
+	 * @param column
+	 * @param d : 방향, 1(가로), 2(세로), 3(대각선)
+	 */
+	private static void Move(int row, int column, int d) { 
+		if(row == N-1 && column == N-1) {
 			answer++;
 			return;
 		}
 		
-//		switch(d) {
-//		case(1):
-//			if()
-//		case(2):
-//		case(3):
-//		}
+		if(d == 1 || d == 3) { // 가로 방향으로 가는 경우
+			if(column + 1 < N && map[row][column+1] == 0)
+				Move(row, column+1, 1);
+		}
 		
+		if(d == 2 || d == 3) { // 세로 방향으로 가는 경우
+			if(row + 1 < N && map[row+1][column] == 0)
+				Move(row+1, column, 2);
+		}
+		
+		if(d == 1 || d == 2 || d == 3) { // 대각선 방향으로 가는 경우
+			if(row + 1 < N && column + 1 < N && map[row+1][column] == 0 && map[row][column+1] == 0 && map[row+1][column+1] == 0) {
+				Move(row+1, column+1, 3);
+			}
+		}
 	}
 	
 } // end of class
