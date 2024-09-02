d4944ab1ea23935db68e4993fc15abfebeafb0de
김유정 <aabbc0908@naver.com>
2024-09-02 16:57:17 +0900

    commit

--- BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
+++ BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
@@ -8,187 +8,49 @@ import java.util.*;
 public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {	
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-	private static int N; // N*N 맵
+	private static int N;
+	private static int[][] map;
+	private static int[][] pipeDirection;
+	
+	private static int r;
+	private static int c;
+	private static int d; // 파이프의 방향 1 : 가로, 2 : 세로, 3 : 대각선 
 
-	private static char[][] map;
-	private static int[][] isVisited;
-	
-	private static int r1; // 현재 위치
-	private static int c1;
-	private static int r2;
-	private static int c2;
-	
 	private static int answer;
-	
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
 		
-		map = new char[N][N];
+		map = new int[N][N];
+		
 		for(int i = 0; i < N; i++) {
-			String row = br.readLine();
-			for(int j = 0, c = 0; j < N; j++, c+=2) {
-				map[i][j] = row.charAt(c);
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		map[0][0] = '-'; map[0][1] = '-';
+		r = 0; c = 1; // 현재 파이프의 끝점
+		d = 1;
 		
-		isVisited = new int[N][N];
-		r1 = 0; c1 = 0; r2 = 0; c2 = 1;
-		answer = 0;
-		movePipe();
-		
-		System.out.println(answer);
-	} // end of main
+		pipeDirection = new int[N][N];
+		pipeDirection[r][c] = d;
+		Move();
+	}
 
-
-	private static void movePipe() {
-		if(r2 >= N-1 && c2 >= N-1) { // 파이프가 (N,N)에 도착했으면 종료
+	private static void Move() {
+		if(r >= N-1 && c >= N-1) {
 			answer++;
 			return;
 		}
 		
-		for(char[] r : map) {
-			System.out.println(Arrays.toString(r));
-		}
-		System.out.println(r1 + ", " + c1);
-		System.out.println(r2 + ", " + c2);
+//		switch(d) {
+//		case(1):
+//			if()
+//		case(2):
+//		case(3):
+//		}
 		
-		int d = pipeDirection();
-
-		switch(d) { // 파이프 상태에 따라 이동
-		case(1): // 파이프가 가로 방향인 경우
-			if(isVisited[r1][c1] == 0) { // 한 번도 간 적이 없다면
-				if(isPromising(1)) { // 가로 방향으로 이동할 수 있으면
-					
-					isVisited[r1][c1] = 1; // 가로 방향으로 이동했다고 체크
-					
-					map[r1][c1] = '0';
-					c1++; c2++;
-					map[r2][c2] = '-';
-					
-					movePipe(); // 이어서 이동
-				}
-				else System.out.println("가로 이동 실패");
-			}
-		
-			if(!isPromising(1) || isVisited[r1][c1] == 1) { // 더이상 가로 방향으로 이동할 수 없거나 이전에 가로 방향으로 이동한 적이 있다면
-				if(isPromising(3)) {
-					isVisited[r1][c1] = 3;
-					
-					map[r1][c1] = '0';
-					c1++; r2++; c2++;
-					System.out.println(r2 + ", " + c2);
-					map[r2][c2] = '-';
-					
-					movePipe();
-				}
-			}
-
-		case(2): // 파이프가 세로 방향인 경우
-			if(isVisited[r1][c1] == 0) { // 한 번도 간 적이 없다면
-				if(isPromising(2)) { // 세로 방향으로 이동할 수 있으면
-					isVisited[r1][c1] = 2; // 세로 방향으로 이동했다고 체크
-					
-					map[r1][c1] = '0';
-					r1++; r2++;
-					map[r2][c2] = '-';
-					
-					movePipe(); // 이어서 이동
-				}
-			}
-		
-			if(!isPromising(2) || isVisited[r1][c1] == 2) { // 이 전에 세로 방향으로 이동한 적이 있다면
-				if(isPromising(3)) {
-					isVisited[r1][c1] = 3;
-					
-					map[r1][c1] = '0';
-					r1++; r2++; c2++;
-					map[r2][c2] = '-';
-					
-					movePipe();
-				}
-			}
-		case(3): // 파이프가 대각선 방향인 경우
-			if(isVisited[r1][c1] == 0) { // 한 번도 간 적이 없다면
-				if(isPromising(1)) { // 가로 방향으로 이동할 수 있으면
-					isVisited[r1][c1] = 1; // 가로 방향으로 이동했다고 체크
-					
-					map[r1][c1] = '0';
-					r1++; c1++; c2++;
-					map[r2][c2] = '-';
-					
-					movePipe(); // 이어서 이동
-				}
-			}
-		
-			if(!isPromising(1) || isVisited[r1][c1] == 1) { // 가로 방향으로 갈 수 없거나 세로 방향으로 간 적이 없다면
-				if(isPromising(2)) { // 세로 방향으로 이동할 수 있으면
-					isVisited[r1][c1] = 2; // 세로 방향으로 이동했다고 체크
-					
-					map[r1][c1] = '0';
-					r1++; c1++; r2++; 
-					map[r2][c2] = '-';
-					
-					System.out.println("세로방향성공");
-					movePipe(); // 이어서 이동
-				}
-				System.out.println("세로방향실패");
-			}
-		
-			if(!isPromising(1) || !isPromising(2) ||  isVisited[r1][c1] == 1 || isVisited[r1][c1] == 2) { // 이 전에 세로 방향으로 이동한 적이 있다면
-				if(isPromising(3)) {
-					isVisited[r1][c1] = 3;
-					
-					map[r1][c1] = '0';
-					r1++; c1++; r2++; c2++;
-					map[r2][c2] = '-';
-					
-					movePipe();
-				}
-			}
-		}
-	}
-	
-	private static int pipeDirection() {
-		if(map[r1][c1+1] == '-') return 1; // 파이프가 가로 방향인 경우
-		else if(map[r1+1][c1] == '-') return 2; // 파이프가 세로 방향인 경우
-		else return 3; // 파이프가 대각선 방향인 경우
-	}
-	
-	private static boolean isPromising(int d) {
-		switch(d) {
-		case(1):
-			if(c2 + 1 < N-1 && map[r2][c2 + 1] != 1) return true; 
-			switch(pipeDirection()) {
-			case(1):
-				if(c2 + 1 == N-1 && r2 == N-1) return true;
-			case(3):
-				if(c2 + 1 == N-1 && r2 == N-1) return true;
-			}
-			break;
-		case(2):
-			if(r2 + 1 < N-1 && map[r2 + 1][c2] != 1) return true;
-			switch(pipeDirection()) {
-			case(2):
-				System.out.println(c2 + ", " + (r2+1));
-				if(c2 == N-1 && r2 + 1 == N-1) return true; break;
-			case(3):
-				if(c2 + 1 == N-1 && r2 == N-1) return true;
-			}
-			break;
-		case(3):
-			if(map[r2][c2 + 1] != 1 && map[r2+1][c2] != 1 && map[r2+1][c2+1] != 1) return true;
-			switch(pipeDirection()) {
-			case(2):
-				if(c2 == N-1 && r2 + 1 == N-1) return true;
-			case(3):
-				if(c2 + 1 == N-1 && r2 == N-1) return true;
-			}
-			break;
-		}
-		return false;
 	}
 	
 } // end of class
