b0962af5b0c00d219a4148202b0f3ef1152b201d
김유정 <aabbc0908@naver.com>
2024-09-03 16:42:13 +0900

    commit

---
+++ BAEKJOON/src/Main_BAEKJOON_24480_깊이우선탐색2_Silver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_BAEKJOON_24480_깊이우선탐색2_Silver {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
