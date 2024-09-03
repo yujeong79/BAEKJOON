1dca6abe3c0b2bf9248ebbd54d5cf7a7d75d0464
김유정 <aabbc0908@naver.com>
2024-09-03 13:41:45 +0900

    commit

---
+++ BAEKJOON/src/Main_BAEKJOON_24479_깊이우선탐색1_Silver2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_BAEKJOON_24479_깊이우선탐색1_Silver2 {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	private static int N;
+	private static int M;
+	private static int R;
+	
+	private static List<Integer>[] adjList;
+	private static boolean[] isVisited;
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken()); // 정점의 수
+		M = Integer.parseInt(st.nextToken()); // 간선의 수
+		R = Integer.parseInt(st.nextToken()); // 시작 정점
+		
+		isVisited = new boolean[N+1];
+		adjList = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
+			
+			adjList[node1].add(node2);
+			adjList[node2].add(node1);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			Collections.sort(adjList[i]);
+		}
+		
+		DFS(R);
+		for(int i = 1; i <= N; i++) {
+			if(!isVisited[i])
+				System.out.println(0);
+		}
+	}
+
+	private static void DFS(int node) {
+		isVisited[node] = true;
+		System.out.println(node);
+		 
+		for(int w : adjList[node]) {
+			if(!isVisited[w])
+				DFS(w);
+		}
+	}
+}
