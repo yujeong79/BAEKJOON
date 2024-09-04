6a735dd6cca470ad343769f1d0b3c7aca28b8f09
김유정 <aabbc0908@naver.com>
2024-09-05 08:55:11 +0900

    commit

--- BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
+++ BAEKJOON/src/Main_BAEKJOON_17070_파이프옮기기1_Gold5.java
@@ -1,38 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-/**
-<<<<<<< HEAD
- * 시작점하고 끝점을 모두 받아보자
-=======
- * N*N의 map을 만들고 파이프가 있으면 1, 없으면 0이다.
- * isPromising
- * 	가로인 경우(0) : 파이프를 옮겼을 때 벽에 부딪히지 않거나 끝에 닿지 않으면 true
- * 		가로 먼저 해보고 다 갔다가 다시 백해서 돌아올 때 대각선 해보기?
- * 	세로인 경우(1) : 파이프를 옮겼을 때 벽에 부딪히지 않거나 끝에 닿지 않으면 true
- * 		세로 먼저 해보고 다 갔다가 다시 백해서 돌아올 때 대각선 해보기?
- *  대각선인 경우(2) :  파이프를 옮겼을 때 벽에 부딪히지 않으면 true
- * 		가로 먼저 해보고 다 갔다가 다시 백해서 돌아올 때 세로 해보고 대각선 해보기?
->>>>>>> branch 'main' of https://github.com/yujeong79/BAEKJOON.git
- */
-
 public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {	
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	private static int N;
 	private static int[][] map;
 
-<<<<<<< HEAD
 	private static int answer;
-=======
-	private static char[][] map;
-
-	private static int cnt;
-
-	private static int r = 0; // 현재 파이프의 시작점
-	private static int c = 0;
-	
->>>>>>> branch 'main' of https://github.com/yujeong79/BAEKJOON.git
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
@@ -48,7 +23,6 @@ public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {
 		
 		Move(0, 1, 1);
 		
-<<<<<<< HEAD
 		System.out.println(answer);
 	} // end of main
 	
@@ -62,15 +36,7 @@ public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {
 			answer++;
 			return;
 		}
-=======
-		Move(0, 0); // 시작 위치
 		
-//		for(char[] r : map) {
-//			System.out.println(Arrays.toString(r));
-//		}
->>>>>>> branch 'main' of https://github.com/yujeong79/BAEKJOON.git
-		
-<<<<<<< HEAD
 		if(d == 1 || d == 3) { // 가로 방향으로 가는 경우
 			if(column + 1 < N && map[row][column+1] == 0)
 				Move(row, column+1, 1);
@@ -88,49 +54,4 @@ public class Main_BAEKJOON_17070_파이프옮기기1_Gold5 {
 		}
 	}
 	
-=======
-	} // end of main
-
-
-	private static void Move(int i, int j) {
-		if(i == N-1 && j == N-1) {
-			cnt++;
-			return;
-		}
-		
-		switch(pipeDirection()) {
-		case(0): // 가로인 경우
-			if(isPromising(0)) { // 오른쪽으로 한 칸 이동이 가능하다면
-				map[r][c] = '0';
-				map[r][c+2] = '-';
-				c++; // 오른쪽으로 한 칸 이동
-			}
-			
-		case(1): // 세로인 경우
-			isPromising(1);
-		case(2): // 대각선인 경우
-			isPromising(2);
-		}
-		
-	}
-	
-	/**
-	 * 파이프의 특정 방향으로의 이동이 유망한지 반환
-	 * @param d : 방향, 0은 가로 1은 세로 2는 대각선
-	 * @return
-	 */
-	private static boolean isPromising(int d) {
-		
-		return true;
-	}
-	
-	/**
-	 * 파이프의 방향을 반환하는 메소드
-	 */
-	private static int pipeDirection() {
-		if(map[r+1][c] == '-') return 0; // 가로
-		else if (map[r][c+1] == '-') return 1; // 세로
-		else return 2;
-	}
->>>>>>> branch 'main' of https://github.com/yujeong79/BAEKJOON.git
 } // end of class
--- BAEKJOON/src/Main_BAEKJOON_2667_단지번호붙이기_Silver1.java
+++ BAEKJOON/src/Main_BAEKJOON_2667_단지번호붙이기_Silver1.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main_BAEKJOON_2667_단지번호붙이기_Silver1 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
 	
 	// 상 하 좌 우
 	private static int[] dr = {-1, 1, 0, 0};
@@ -10,56 +11,51 @@ public class Main_BAEKJOON_2667_단지번호붙이기_Silver1 {
 	
 	private static int N;
 	private static int[][] map;
+	//private static boolean[][] isVisited;
 	
+	private static int cnt;
 	private static List<Integer> answer;
+	
 
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
 		
+		//isVisited = new boolean[N+2][N+2];
 		map = new int[N+2][N+2];
 		for(int i = 1; i <= N; i++) {
 			String row = br.readLine();
 			for(int j = 1; j <= N; j++) {
-				map[i][j] = row.charAt(j) - '0';
+				map[i][j] = row.charAt(j-1) - '0';
 			}
 		}
 		
 		answer = new ArrayList<>();
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= N; j++) {
-				if(map[i][j] != 0)
-					BFS(i, j);	
-			}
-		}
-		
-		System.out.println(answer.toString());
-		
-	} // end of main
-
-	private static void BFS(int i, int j) {
-		int[] start = {i, j};
-		Queue<int[]> queue = new LinkedList<>();
-		
-		queue.add(start);
-		map[i][j] = 0;
-		int length = 1;
-		
-		while(!queue.isEmpty()) {
-			int[] curr = queue.poll();
-			int r = curr[0];
-			int c = curr[1];
-			if(map[r+dr[0]][c+dc[0]] != 0 || map[r+dr[1]][c+dc[1]] != 0 || map[r+dr[2]][c+dc[2]] != 0 || map[r+dr[3]][c+dc[3]] != 0) {
-				for(int d = 0; d < 4; d++) {
-					if(map[r+dr[i]][c+dc[i]] != 0) {
-						int[] adj = {r+dr[i], c+dc[i]};
-						queue.add(adj); length++;
-						map[r+dr[i]][c+dc[i]] = 0;
-					}
+				if(map[i][j] != 0) {
+					cnt = 0; // 각 단지에 속하는 집의 수
+					DFS(i, j);
+					answer.add(cnt);
 				}
 			}
 		}
 		
-		answer.add(length);
-		
+		sb.append(answer.size() + "\n");
+		for(int n : answer) {
+			sb.append(n + "\n");
+		}
+		System.out.println(sb);
 	} // end of main
+
+	private static void DFS(int i, int j) {
+		map[i][j] = 0; cnt++;
+		
+		for(int d = 0; d < 4; d++) {
+			if(map[i+dr[d]][j+dc[d]] != 0) {
+				DFS(i+dr[d], j+dc[d]);
+			}
+		}
+		
+	}
+	
 } // end of class
