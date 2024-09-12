70c396b4a6045c0542333145cbff4b9782539e29
김유정 <aabbc0908@naver.com>
2024-09-12 13:45:28 +0900

    commit

---
+++ BAEKJOON/src/Main_BAEKJOON_2636_치즈_Gold4_000ms.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_BAEKJOON_2636_치즈_Gold4_000ms {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
