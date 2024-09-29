f7ef958071126aaf2fec97f1d28d06bdbd777a47
yujeong <yujeong@localhost>
2024-09-29 22:07:00 +0900

    commit

---
+++ BAEKJOON/src/Main_BAEKJOON_9084_동전_000ms.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main_BAEKJOON_9084_동전_000ms {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int money = sc.nextInt();
+		int[] dp = new int[money+1];
+		
+		for(int i = 1; i <= money; i++) {
+			int minCnt = Integer.MAX_VALUE;
+			
+			
+		}
+	
+	} // end of main
+} // end of class
