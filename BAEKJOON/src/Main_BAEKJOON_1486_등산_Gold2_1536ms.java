190732d5efe6f48f64df6c0c0ccf258267a109b0
yujeong <yujeong@localhost>
2024-09-29 11:32:12 +0900

    commit

--- BAEKJOON/src/Main_BAEKJOON_1486_등산_Gold2_1536ms.java
+++ BAEKJOON/src/Main_BAEKJOON_1486_등산_Gold2_1536ms.java
@@ -2,15 +2,9 @@ import java.io.*;
 import java.util.*;
 
 /**
- * A(0) ~ Z(25), a(26) ~ z(51)
- * 현재 위치에서 바로 인접한 정수 좌표 중 높이의 차이가 T보다 크지 않은 곳으로만 이동 가능
- * 	- 낮은 곳이나 같은 곳으로 이동하면 1초 소요
- * 	- 높은 곳으로 이동한다면 두 위치의 높이 차의 제곱 소요
- * D보다 크지 않은 시간 동안 올라갈 수 있는 최대 높이를 구하기
- * 
- * BFS랑 합쳐야하나?
- * 
+ * 방법1. 등산할 때의 최소 시간을 구하는 다익스트라와 하산할 때의 최소 시간을 구하는 다익스트라를 따로 구해서 둘이 합치는 방법
  */
+
 public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
@@ -24,15 +18,16 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken()); // 인접한 좌표와의 차이가 T 이내이면 이동 가능
-		D = Integer.parseInt(st.nextToken()); // 어두워지는 시간
+		T = Integer.parseInt(st.nextToken()); 
+		D = Integer.parseInt(st.nextToken());
 		
-		INF = D + 1;
-		lastPoint = N*M;
-		map = new int[N][M];
+		INF = D + 1; // 걸리는 시간을 초기화 하기 위한 무한대의 값, Integer.MAX_VALUE로 설정하니까 (등산 시간+하산 시간)을 했을 때 값이 음수로 바뀌는 문제가 있어서 D+1로 초기화	
+		lastPoint = N*M; // 자주 사용하므로 전체 map 칸의 개수를 저장
+		
 		// 알파벳으로 지도를 저장하면 비교할 때 헷갈리니까 그냥 아예 수로 바꿔서 저장해버리자
 		// 그런데 또 어차피 char 간에는 등호 연산이 가능한데 수로 바꾸지 말까? 하다가 소문자랑 대문자 간의 이동 가능 유무를 판단하기 위해서는 바꿔야겠다고 생각함
 		// A : 65, Z : 90, a : 97, z : 122
+		map = new int[N][M];
 		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
 			for(int j = 0; j < M; j++) {
@@ -41,25 +36,17 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 			}
 		}
 		
-		// 다익스트라로 풀기 위해서는
-		// 1. 현 위치에서 갈 수 있는 다음 위치가 어디인지를 알아야하고
-		// 2. 시작 지점에서부터 갈 수 있는 위치들까지 걸리는 최소 시간을 저장해야하고
-		climbUpTime = new int[N][M];
-		climbDownTime = new int[N][M];
-		for(int i = 0; i < N; i++) {
+		climbUpTime = new int[N][M]; // 호텔에서 각 지점까지 등산하는데 걸리는 시간을 저장하기 위한 2차원 배열
+		climbDownTime = new int[N][M]; // 각 지점에서 호텔까지 하산하는데 걸리는 시간을 저장하기 위한 2차원 배열
+		
+		for(int i = 0; i < N; i++) { // 다익스트라1. 각 지점마다 걸리는 시간을 무한대로 초기화
 			Arrays.fill(climbUpTime[i], INF);
 		}
 		
-		
-		
 		climbUp(0);
-//		System.out.println("등산");
-//		for(int[] t : climbUpTime) {
-//			System.out.println(Arrays.toString(t));
-//		}
 		
 		int highest = 0;
-		for(int i = 0; i < N*M; i++) {
+		for(int i = 0; i < N*M; i++) { // !!! 이렇게 0부터 모든 좌표의 하산 시간을 계산하는 것은 낭비 아닐까?, 가장 높은 위치에서부터 계산해서 D 시간 이내이면 멈추게 해볼까?	
 			climbDown(i);
 			
 			int R = i / M;
@@ -68,11 +55,6 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 			if((climbUpTime[R][C] + climbDownTime[0][0]) >= 0 && (climbUpTime[R][C] + climbDownTime[0][0]) <= D && map[R][C] > highest) {
 				highest = map[R][C];
 			}
-			
-//			System.out.println(i + "에서 하산하면 " + (climbUpTime[R][C]+climbDownTime[0][0]) + "시간 소요");
-//			for(int[] t : climbDownTime) {
-//				System.out.println(Arrays.toString(t));
-//			}
 		}
 		
 		
@@ -81,17 +63,21 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 		
 	} // end of main
 
+	// climbUp과 climbDown도 얕은 복사를 사용해서 두 메소드를 하나로 합칠 수는 없을까?	
+	// 해결해야하는 문제 1. 시작 위치를 매개변수로 넘겨주어야 한다.
+	// 해결해야하는 문제 2. 등산인지 하산인지에 따라 걸리는 시간을 저장할 2차원 배열을 다르게 지정해주어야 한다.	
 	private static void climbDown(int departure) {
-		for(int i = 0; i < N; i++) {
+		for(int i = 0; i < N; i++) { // 하산은 하산 시작 지점이 매번 달라지므로 climbDown에서 무한대로 초기화
 			Arrays.fill(climbDownTime[i], INF);
 		}
 		
 		int R = departure/M;
 		int C = departure%M;
-		climbDownTime[R][C] = 0;
+		climbDownTime[R][C] = 0; // 시작지점을 0으로 갱신
 		
 		boolean[][] isVisited = new boolean[N][M];
 		
+		// 아래서부터는 다익스트라 알고리즘
 		for(int i = 0; i < lastPoint; i++) {
 			int min = INF;
 			int curr = -1;
@@ -111,7 +97,7 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 			C = curr % M;
 			isVisited[R][C] = true;
 			
-			for(int d = 0; d < 4; d++) {
+			for(int d = 0; d < 4; d++) { // 인접한 좌표 중에서
 				int nr = R + dir[d][0];
 				int nc = C + dir[d][1];
 				if(nr >= 0 && nr < N && nc >= 0 && nc < M) {
@@ -120,7 +106,7 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 							if(climbDownTime[nr][nc] > climbDownTime[R][C] + Math.pow(map[nr][nc]-map[R][C], 2)) {
 								climbDownTime[nr][nc] = (int) (climbDownTime[R][C] + Math.pow(map[nr][nc]-map[R][C], 2));
 							}
-						} else {
+						} else { // 현 위치보다 낮다면 1초 증가
 							if(climbDownTime[nr][nc] > climbDownTime[R][C] + 1) {
 								climbDownTime[nr][nc] = climbDownTime[R][C] + 1;
 							}
@@ -167,7 +153,7 @@ public class Main_BAEKJOON_1486_등산_Gold2_1536ms {
 							if(climbUpTime[nr][nc] > climbUpTime[R][C] + Math.pow(map[nr][nc]-map[R][C], 2)) {
 								climbUpTime[nr][nc] = (int) (climbUpTime[R][C] + Math.pow(map[nr][nc]-map[R][C], 2));
 							}
-						} else {
+						} else { // 현 위치보다 낮다면 1초 증가
 							if(climbUpTime[nr][nc] > climbUpTime[R][C] + 1) {
 								climbUpTime[nr][nc] = climbUpTime[R][C] + 1;
 							}
