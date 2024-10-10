b1bc72b50e70f9282d5332fd31a55a4db106ce9b
김유정 <aabbc0908@naver.com>
2024-10-10 17:22:27 +0900

    commit

--- BAEKJOON/src/Main_3649_로봇프로젝트.java
+++ BAEKJOON/src/Main_3649_로봇프로젝트.java
@@ -6,35 +6,45 @@ import java.util.*;
  */
 public class Main_3649_로봇프로젝트 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-	static final int nanometer = 10_000_000;
-	static int x, n, maxDiff;
+	static int x, n;
 	static int[] lego;
+	
 	public static void main(String[] args) throws IOException {
-		x = Integer.parseInt(br.readLine()); // 구멍의 너비 (1 ≤ x ≤ 20)
-		n = Integer.parseInt(br.readLine()); // 레조 조각의 수 (0 ≤ n ≤ 1000000)
-		
-		lego = new int[n];
-		for(int i = 0; i < n; i++) {
-			lego[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(lego);
-		maxDiff = 0;
-		
-		int L = 0;
-		int R = n-1;
-		while(L <= R) {
-			int sum = lego[L]+lego[R];
+		String firstInput = null;
+		while((firstInput=br.readLine()) != null) {
+			x = Integer.parseInt(firstInput)*10000000; // 구멍의 너비 (1 ≤ x ≤ 20)
+			n = Integer.parseInt(br.readLine()); // 레조 조각의 수 (0 ≤ n ≤ 1,000,000)
 			
-			if(sum/nanometer == x && sum%nanometer == 0) {
-				maxDiff = Math.max(maxDiff, Math.abs(lego[L]-lego[R]));
-				L++;
-				R--;
-			} else {
-				
+			lego = new int[n];
+			for(int i = 0; i < n; i++) {
+				lego[i] = Integer.parseInt(br.readLine());
 			}
-		}
-		
+			
+			Arrays.sort(lego);
+			
+			boolean flag = false;
+			
+			int L = 0;
+			int R = n-1;
+			while(L < R) {
+				int sum = lego[L]+lego[R];
+				
+				if(sum == x) { // 두 조각이 구멍의 너비 x를 완벽하게 막으면
+					System.out.println("yes " + lego[L] + " " + lego[R]);
+					flag = true;
+					break;
+				} else { // 막지 못하는 경우
+					if(sum > x) { //  두 조각의 합이 구멍의 너비보다 큰 경우
+						R--;
+					} else { // 두 조각의 합이 구멍의 너비보다 작은 경우
+						L++;
+					}
+				}
+			}
+			
+			if(!flag) System.out.println("danger");
+			
+			firstInput = null;
+		} // end of testCase
 	} // end of main
 } // end of class
