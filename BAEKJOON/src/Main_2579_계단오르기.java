e4f9843fc7d0b1df2293664f16f0e3f66701ad51
yujeong <aabbc0908@naver.com>
2024-10-13 21:02:57 +0900

    commit

---
+++ BAEKJOON/src/Main_2579_계단오르기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_2579_계단오르기 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) {
+		
+	} // end of main
+} // end of class
