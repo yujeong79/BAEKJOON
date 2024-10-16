4278572a71c78d4c20ef6c9a50b527bad7bd5e41
yujeong <aabbc0908@naver.com>
2024-10-16 22:09:34 +0900

    commit

--- BAEKJOON/src/Main_16236_아기상어.java
+++ BAEKJOON/src/Main_16236_아기상어.java
@@ -2,33 +2,25 @@ import java.io.*;
 import java.util.*;
 
 /**
+ * 1. BFS로 가장 가까운 물고기를 찾아서 리스트에 넣는다.
+ * 	거리가 가까운 물고기가 많다면 가장 위에 있으면서 가장 왼쪽에 있는 순으로 정렬하여 첫번째 물고기를 먹는다.
  * 
- * 맨 처음 아기상어의 크기는 2
+ * 2. 이동한 거리만큼 time을 더한다.
+ * 3. 다시 그 위치부터 BFS 탐색을 시작한다.	(물고기를 다 먹었거나 BFS를 돌렸는데 먹을 수 있는 물고기가 없는 경우 엄마 상어에게 도움을 요청한다.)
  * 
- * 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
- * 먹을 수 있는 물고기가 2마리 이상이라면, 거리가 가장 가까운 물고기를 먹으러 간다.
- * 	- 거리가 가까운 물고기가 많다면 가장 위에 있는 물고기 -> 가장 왼쪽에 있는 물고기 순으로 먹는다.
- * 
- *  아기 상어가 1칸 이동하는데 1초가 걸린다.
- *  
- *  아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없다.
- *  아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 잇다.
- *  아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때마다 크기가 1 증가한다.
- *  
- *  => 더 이상 먹을 수 없는 물고기가 공간에 없다면 아기 상어는 엄마 상어를 부른다.
- *  	1. 물고기가 더 이상 없는 경우
- *  	2. 남은 물고기의 크기가 자신의 크키와 같거나 큰 경우
  */
 
 public class Main_16236_아기상어 {
 	static class Fish implements Comparable<Fish>{
-		int r, c, size, cnt;
+		int r, c, size, dist, cnt;
 		
 		public Fish() {}
 		
-		public Fish(int r, int c) {
+		public Fish(int r, int c, int size, int dist) {
 			this.r = r;
 			this.c = c;
+			this.size = size;
+			this.dist = dist;
 		}
 
 		@Override
@@ -36,10 +28,16 @@ public class Main_16236_아기상어 {
 			if(this.r == o.r) return this.c - o.c;
 			return this.r - o.r;
 		}
+
+		@Override
+		public String toString() {
+			return "(" + r + ", " + c + ")";
+		}
+		
 	}
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int N, fishCnt;
+	static int N, fishCnt, callMom;
 	static int[][] map;
 	static Fish shark;
 	
@@ -54,6 +52,7 @@ public class Main_16236_아기상어 {
 			for(int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if(map[i][j] == 9) { // 상어의 위치와 사이즈 저장
+					map[i][j] = 0;
 					shark.r = i;
 					shark.c = j;
 					shark.size = 2;
@@ -61,39 +60,45 @@ public class Main_16236_아기상어 {
 			}
 		}
 		
-		BFS(shark.r, shark.c);
+		while(fishCnt > 0) {
+			if(!BFS(shark.r, shark.c)) { // 현재 상어의 위치에서부터 탐색해서 먹을 수 있는 물고기가 이제 없다면
+				break;
+			}
+		}
+		
+		System.out.println(callMom);
 		
 	} // end of main
 
 	private static final int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
-	private static void BFS(int r, int c) {
+	private static boolean BFS(int r, int c) {
 		boolean[][] isVisited = new boolean[N][N];
 		List<Fish> eatableFish = new ArrayList<>(); // 먹을 수 있는 물고기의 좌표를 저장하자
 		
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {r, c}); // 처음 상어의 위치는 0으로 해두자
+		Queue<Fish> queue = new LinkedList<>();
+		queue.add(new Fish(r, c, 0, 0)); // 처음 상어의 위치는 물고기가 없으므로 size를 0으로 하자
 		isVisited[r][c] = true;
 		
 		while(!queue.isEmpty()) {
 			int qSize = queue.size();
 			
 			for(int i = 0; i < qSize; i++) {
-				int[] curr = queue.poll();
+				Fish curr = queue.poll();
 				
 			
 				for(int d = 0; d < 4; d++) {
-					int nr = curr[0] + dir[d][0];
-					int nc = curr[1] + dir[d][1];
+					int nr = curr.r + dir[d][0];
+					int nc = curr.c + dir[d][1];
 					
 					if(nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] <= shark.size && !isVisited[nr][nc]) {
-						queue.add(new int[] {nr, nc});
+						Fish next = new Fish(nr, nc, map[nr][nc], curr.dist+1);
+						queue.add(next);
 						isVisited[nr][nc] = true;
 						
 						if(map[nr][nc] >= 1 && map[nr][nc] < shark.size) { // 먹을 수 있는 물고기면
-							eatableFish.add(new Fish(nr, nc)); // 먹을 수 있는 목록에 추가
+							eatableFish.add(next); // 먹을 수 있는 목록에 추가
 						}
-						
 					}
 				}
 			}
@@ -102,14 +107,23 @@ public class Main_16236_아기상어 {
 				Collections.sort(eatableFish);
 				
 				Fish fish = eatableFish.get(0);
+				
 				map[fish.r][fish.c] = 0; // 아기상어가 먹어서 없앰
 				shark.r = fish.r;
 				shark.c = fish.c;
 				shark.cnt++; // 먹은 물고기의 수 1 증가
 				
-				if(shark.cnt == shark.size) shark.size++; // 자신의 크기 만큼 물고기를 먹으면 크기가 1 증가한다.
+				if(shark.cnt == shark.size) {
+					shark.size++; // 자신의 크기 만큼 물고기를 먹으면 크기가 1 증가한다.
+					shark.cnt = 0;
+				}
+				
+				callMom += fish.dist;
+				return true;
 			}
 		}
+		
+		return false;
 	}
 	
 } // end of class
