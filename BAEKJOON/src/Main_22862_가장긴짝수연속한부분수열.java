457a0635ade201bd8f27e6007292f949674b888f
김유정 <aabbc0908@naver.com>
2024-10-17 14:10:47 +0900

    commit

---
+++ BAEKJOON/src/Main_22862_가장긴짝수연속한부분수열.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 두 포인터를 사용해서 
+ */
+
+public class Main_22862_가장긴짝수연속한부분수열 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N, K;
+	static List<Integer> nums;
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken()); // 수열의 길이, (1 <= N <= 1,000,000)
+		K = Integer.parseInt(st.nextToken()); // 삭제할 수 있는 최대 횟수, (1 <= K <= 100,000)
+		
+		
+		
+		
+		
+	} // end of main
+} // end of class
