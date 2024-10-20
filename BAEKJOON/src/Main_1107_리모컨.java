d45ab2cc60e9f4479e3ac2d65e77702dbb1b5d39
김유정 <aabbc0908@naver.com>
2024-10-21 08:40:11 +0900

    commit

--- BAEKJOON/src/Main_1107_리모컨.java
+++ BAEKJOON/src/Main_1107_리모컨.java
@@ -1,41 +1,51 @@
 import java.io.*;
-import java.util.*;
-
-/**
- * 수빈이가 현재 보고 있는 채널은 100번
- */
 
 public class Main_1107_리모컨 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int[] N;
-	static int M, curr, minCnt;
+	static int N, M, currChannelInt, minCnt;
 	static boolean[] buttons = new boolean[10];
 	
 	public static void main(String[] args) throws IOException {
-		String str = br.readLine();
-		int nSize = str.length();
-		
-		N = new int[nSize];
-		
-		for(int i = 0; i < nSize; i++) {
-			N[i] = str.charAt(i)-'0';
-		}
-		
+		N = Integer.parseInt(br.readLine()); // 이동하려고 하는 채널
 		M = Integer.parseInt(br.readLine()); // 고장난 버튼의 개수
-		
-		Arrays.fill(buttons, true);
 		
 		if(M > 0) {
 			String brokenButtons = br.readLine();
-			for(int i = 0; i < M; i++) {
-				int idx = brokenButtons.charAt(i*2)-'0';
-				buttons[idx] = false;
+			for(int i = 0, ci = 0; i < M; i++, ci += 2) {
+				int idx = brokenButtons.charAt(ci)-'0';
+				buttons[idx] = true;
 			}
 		}
 		
-		// 사용할 수 있는 버튼을 사용해서 최대한 N과 가까운 숫자를 만들어보자.
+		minCnt = Integer.MAX_VALUE;
+		currChannelInt = 0;
+		
+		while(currChannelInt <= 1000000) {
+			String currChannelString = currChannelInt+"";
+			int currChannelLength = currChannelString.length();
+			boolean flag = true;
+			
+			for(int i = 0; i < currChannelLength; i++) {
+				int idx = currChannelString.charAt(i) - '0';
+				if(buttons[idx]) {
+					flag = false;
+					break;
+				}
+			}
+			
+			if(flag) {
+				int cnt = Math.abs(N - currChannelInt) + currChannelLength;
+				if(minCnt < cnt) break;
+				else minCnt = cnt;
+			}
+			
+			currChannelInt++;
+		}
 		
 		
+		minCnt = Math.min(Math.abs(minCnt), Math.abs(N-100));
+		
+		System.out.println(minCnt);
 		
 	} // end of main
 } // end of class
