0226d924aef8da937ba16f081ecd8c026584f5df
김유정 <aabbc0908@naver.com>
2024-10-22 14:38:08 +0900

    commit

--- BAEKJOON/src/Main_2573_빙산.java
+++ BAEKJOON/src/Main_2573_빙산.java
@@ -7,9 +7,10 @@ import java.util.*;
 
 public class Main_2573_빙산 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int N, M, year;
+	static int N, M, year, icebergCnt;
 	static int[][] map;
-	static boolean[][] iceberg;
+	static boolean[][] iceberg, isVisited;
+	static boolean isDivided;
 	
 	public static void main(String[] args) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 3 <= N, M <= 300
@@ -24,16 +25,36 @@ public class Main_2573_빙산 {
 			for(int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if(map[i][j] != 0) {
+					icebergCnt++;
 					iceberg[i][j] = true; // 해당 좌표가 빙산이면 true
 				}
 			}
 		}
 		
-		findIceberg();
+		year = 0;
+		isDivided = false;
 		
 		countArea();
 		
+		while(!isDivided && icebergCnt > 0) {
+//			System.out.println(icebergCnt);
+			
+			findIceberg();
+			
+//			for(int[] m : map) {
+//				System.out.println(Arrays.toString(m));
+//			}
+			
+//			System.out.println(icebergCnt);
+			if(icebergCnt > 0) {
+				countArea();
+				year++;
+			}
+			
+		}
 		
+		if(icebergCnt == 0 && !isDivided) System.out.println(0);
+		else System.out.println(year);
 		
 	} // end of main
 	
@@ -41,16 +62,18 @@ public class Main_2573_빙산 {
 	
 	private static void countArea() {
 		int cnt = 0;
+		isVisited = new boolean[N][M];
 		
 		for(int i = 1; i < N-1; i++) {
 			for(int j = 1; j < M-1; j++) {
-				if(map[i][j] > 0 && !isVisited[i][i]) {
+				if(map[i][j] > 0 && !isVisited[i][j]) {
 					
-					if(cnt > 0) {
+					if(cnt >= 1) {
+						isDivided = true;
 						return;
 					}
 					
-					BFS();
+					BFS(i, j);
 					cnt++;
 				} else if(map[i][j] == 0 && iceberg[i][j]) { // 빙산이 다 녹았으면 해당 좌표를 false로 변경
 					iceberg[i][j] = false;
@@ -60,8 +83,25 @@ public class Main_2573_빙산 {
 		
 	}
 
-	private static void BFS() {
-		// TODO Auto-generated method stub
+	private static void BFS(int r, int c) {
+		Queue<int[]> queue = new LinkedList<>();
+		
+		queue.add(new int[] {r, c});
+		isVisited[r][c] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			
+			for(int d = 0; d < 4; d++) {
+				int nr = curr[0] + dir[d][0];
+				int nc = curr[1] + dir[d][1];
+				
+				if(map[nr][nc] > 0 && !isVisited[nr][nc]) {
+					queue.add(new int[] {nr, nc});
+					isVisited[nr][nc] = true;
+				}
+			}
+		}
 		
 	}
 
@@ -80,10 +120,13 @@ public class Main_2573_빙산 {
 			int nr = r + dir[d][0];
 			int nc = c + dir[d][1];
 			
-			if(map[nr][nc] > 0 && !iceberg[nr][nc]) { // 인접한 좌표가 바다라면, 기존 빙하였던 자리가 0이 되어 바다와 구분이 되지 않는 것을 방지하기 위해서 iceberg 배열 사용
-				map[nr][nc]--;
+			if(map[nr][nc] == 0 && !iceberg[nr][nc]) { // 인접한 좌표가 바다라면, 기존 빙하였던 자리가 0이 되어 바다와 구분이 되지 않는 것을 방지하기 위해서 iceberg 배열 사용
+				if(map[r][c] > 0)
+					map[r][c]--;
+				
 			}
 		}
+		if(map[r][c] == 0) icebergCnt--;
 	}
 
 	
