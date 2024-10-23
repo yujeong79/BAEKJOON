9b2a87a9411da735eda3f330ba5561f748bbfdaf
김유정 <aabbc0908@naver.com>
2024-10-24 08:45:31 +0900

    commit

--- BAEKJOON/src/Main_10816_숫자카드2.java
+++ BAEKJOON/src/Main_10816_숫자카드2.java
@@ -1,18 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * -10 -10 2 3 3 3 7 10 10 10
- *  
- */
-
 public class Main_10816_숫자카드2 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	
 	static int N, M;
 	static int[] cards;
-	static int[] nums;
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine()); // 상근이가 가지고 있는 숫자 카드의 개수
@@ -26,12 +20,11 @@ public class Main_10816_숫자카드2 {
 		Arrays.sort(cards);
 		
 		M = Integer.parseInt(br.readLine());
-		nums = new int[M];
 		
 		/** M개의 숫자를 입력 받음과 동시에 해당 숫자 카드가 몇 개가 있는지 동시에 세자! */
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < M; i++) {
-			count(nums[i]);
+			count(Integer.parseInt(st.nextToken()));
 		}
 		
 		System.out.println(sb);
@@ -45,23 +38,23 @@ public class Main_10816_숫자카드2 {
 		while(start <= end) {
 			int mid = (start + end) / 2;
 			
-			if(cards[mid] > num) {
-				end = mid - 1;
-			}
-			
-			else if(cards[mid] < num) {
-				start = mid + 1;
-			}
-			
-			else if(cards[mid] == num){
-				int L = mid;
-				int R = mid;
+			if(cards[mid] == num){
+				int L = mid-1;
+				int R = mid+1;
 				
 				while(L >= 0 && cards[L] == num) L--;
 				while(R < N && cards[R] == num) R++;
 				
 				sb.append((R-L-1) + " ");
 				return;
+			}
+			
+			else if(cards[mid] > num) {
+				end = mid - 1;
+			}
+			
+			else if(cards[mid] < num) {
+				start = mid + 1;
 			}
 		}
 		
