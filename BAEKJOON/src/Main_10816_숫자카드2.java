117827ff6e7f613e2d6f4551efdbde1cd1007a8e
김유정 <aabbc0908@naver.com>
2024-10-24 12:13:59 +0900

    commit

--- BAEKJOON/src/Main_10816_숫자카드2.java
+++ BAEKJOON/src/Main_10816_숫자카드2.java
@@ -10,12 +10,14 @@ public class Main_10816_숫자카드2 {
 	
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine()); // 상근이가 가지고 있는 숫자 카드의 개수
-		cards = new int[N];
+		cards = new int[N+1]; 
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < N; i++) {
 			cards[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		cards[N] = Integer.MAX_VALUE;
 		
 		Arrays.sort(cards);
 		
@@ -24,45 +26,47 @@ public class Main_10816_숫자카드2 {
 		/** M개의 숫자를 입력 받음과 동시에 해당 숫자 카드가 몇 개가 있는지 동시에 세자! */
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < M; i++) {
-			count(Integer.parseInt(st.nextToken()));
+			findIdx(Integer.parseInt(st.nextToken()));
 		}
 		
 		System.out.println(sb);
 		
 	} // end of main
 
-	private static void count(int num) {
+	// 해당 숫자의 가장 왼쪽 인덱스와 가장 오른쪽 인덱스를 찾자
+	private static void findIdx(int num) {
 		int start = 0;
-		int end = N-1;
+		int end = N;
+		int mid = 0;
 		
-		while(start <= end) {
-			int mid = (start + end) / 2;
+		int leftIdx = 0;
+		while(start < end) {
+			mid = (start+end)/2;
 			
-			if(cards[mid] == num){
-				int L = mid-1;
-				int cnt = 0;
-				
-				// 여기서 어떻게 while문을 안씀?
-				while(L >= 0 && cards[L] == num) L--; // num과 같은 숫자 중 가장 왼쪽의 위치를 찾아서
-				
-				while(++L < N && cards[L] == num) {
-					cnt++;
-				}
-				
-				sb.append(cnt + " ");
-				return;
-			}
-			
-			else if(cards[mid] > num) {
-				end = mid - 1;
-			}
-			
-			else if(cards[mid] < num) {
-				start = mid + 1;
-			}
+			/** num과 같은 경우에 start를 올려주게 되면 안되니까!!
+			 *  가장 왼쪽 인덱스는 해당 배열에서 처음으로 num값이 나오는 자리에 들어가야하므로
+			 *  end를 num과 같은 경우 mid를 가리키게 한다.
+			 */
+			if(cards[mid] >= num) end = mid;
+			else start = mid + 1;
 		}
 		
-		sb.append("0 ");
+		leftIdx = start;
+		
+		start = 0;
+		end = N;
+		
+		int rightIdx = N;
+		while(start < end) {
+			mid = (start+end)/2;
+			
+			if(cards[mid] <= num) start = mid + 1;
+			else end = mid;
+		}
+		
+		rightIdx = end;
+		
+		sb.append((rightIdx-leftIdx) + " ");
 		
 	}
 	
