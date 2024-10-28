8d751fc156efd7a334fa4869aa596ced2a01c61c
yujeong <aabbc0908@naver.com>
2024-10-28 21:43:00 +0900

    commit

--- BAEKJOON/src/Main_19637_IF문좀대신써줘.java
+++ BAEKJOON/src/Main_19637_IF문좀대신써줘.java
@@ -1,54 +1,78 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 범위만 기억할까 아니면 배열을 만들까
+ * 이렇게 생각하면 범위만 기억하는게 훨씬 낫긴 낫겠다.
+ * 
+ * 위의 방법처럼 하니까 시간초과가 나네...
+ */
+
 public class Main_19637_IF문좀대신써줘 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	
-	static Map<String, Integer> map;
-	static int N, M;
+	static int N, M, vSize;
 	static int[] character;
-	static String[] titles;
+	static String[] key;
+	static int[] value;
 	
 	public static void main(String[] args) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken()); // 칭호의 개수
 		M = Integer.parseInt(st.nextToken()); // 캐릭터의 개수
 		
-		map = new HashMap<>();
+		key = new String[N];
+		value = new int[N];
+		
+		int former = 0; // 이전의 power를 저장
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			String title = st.nextToken();
-			int power = Integer.parseInt(st.nextToken());
+			String k = st.nextToken();
+			int v = Integer.parseInt(st.nextToken());
 			
-			map.put(title, power);
+			key[i] = k;
+			value[i] = v;
+				
 		}
 		
-		character = new int[M];
+		character = new int[M+1];
 		for(int i = 0; i < M; i++) {
 			character[i] = Integer.parseInt(br.readLine());
+			
+			binarySearch(character[i]);
 		}
 		
-		Arrays.sort(character);
-		
-		for(String key : map.keySet()) {
-			binarySearch(map.get(key));
-		}
-		
+		System.out.println(sb);
 		
 	} // end of main
 
-	private static void binarySearch(Integer power) {
+	/**
+	 * 현재 캐릭터의 칭호를 찾는 메서드
+	 * @param curr : 현재 캐릭터
+	 */
+	private static void binarySearch(int curr) {
 		int start = 0;
-		int end = M-1;
+		int end = N - 1;
+		
+		int result = 0;
 		
 		while(start <= end) {
 			int mid = (start+end)/2;
+				
+			if(value[mid] >= curr) {
+				end = mid - 1;
+				result = mid;
+			}
 			
-			if(character[mid] > power) end = mid - 1;
+			else if(value[mid] < curr) {
+				start = mid + 1;
+			}
 		}
 		
+		sb.append(key[start] + "\n");
+
 	}
 	
 } // end of class
