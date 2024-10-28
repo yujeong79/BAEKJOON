11e297d1413d866a8d549cc4d6b16d3890d3154b
yujeong <aabbc0908@naver.com>
2024-10-28 21:58:48 +0900

    Commit

---
+++ BAEKJOON/src/Main_2110_공유기설치.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_2110_공유기설치 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, C;
+    static int[] house;
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken()); // 집의 개수, (2 ≤ N ≤ 200,000)
+        C = Integer.parseInt(st.nextToken()); // 공유기의 개수, (2 ≤ C ≤ N)
+
+
+    } // end of main
+} // end of class
