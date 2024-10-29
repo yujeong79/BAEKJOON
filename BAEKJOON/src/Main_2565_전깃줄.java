f55e2f6ec29cad2f42a8610feb9d2e04a9c7b03f
김유정 <aabbc0908@naver.com>
2024-10-29 17:51:45 +0900

    commit

---
+++ BAEKJOON/src/Main_2565_전깃줄.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_2565_전깃줄 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine()); // 두 전봇대 사이의 전깃줄의 개수
+		
+	} // end of main
+} // end of class
