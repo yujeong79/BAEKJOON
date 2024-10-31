7c729048bc1b1a3434f79598fd507e911723e2ab
김유정 <aabbc0908@naver.com>
2024-11-01 08:56:01 +0900

    commit

--- BAEKJOON/src/Main_22871_징검다리건너기.java
+++ BAEKJOON/src/Main_22871_징검다리건너기.java
@@ -17,22 +17,26 @@ public class Main_22871_징검다리건너기 {
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
 		
+		stones = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < N; i++) {
 			stones[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		// 최솟값이랑 최댓값을 뭐로 할까......................... 뭐로 할까욧!
-		// 그런데 DP로 푸는게 더 쉬울거 같은데??????
+		// 그런데 DP로 푸는게 더 쉬울거 같은데?????? DP로 최솟값과 최댓값을 구하고 이분탐색을 해야하나?
 		dp = new int[N];
 		dp[0] = 0;
 		
-		for(int i = 1; i < N; i++) {
-			dp[i] = Math.abs(stones[i-1] - stones[i]) + 1; 
-			for(int j = 0; j < i; j++) {
-				dp[i] = Math.min((j-i) * (Math.abs(stones[i-1] - stones[i]) + 1), dp[i]);
+		for(int j = 1; j < N; j++) {
+			dp[j] = Math.abs(stones[j-1] - stones[j]) + 1; // 이전 칸에서 다음 칸으로 이동할 때 쓰는 힘 
+			for(int i = 0; i < j; i++) {
+				int result = (j-i) * (Math.abs(stones[i] - stones[j]) + 1);
+				dp[j] = Math.min(result, dp[j]);
 			}
 		}
+		
+		System.out.println(Arrays.toString(dp));
 		
 		
 		
