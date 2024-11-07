5ea27188a12e5f04d522635ab83c05983eb1caa1
김유정 <aabbc0908@naver.com>
2024-11-07 17:39:21 +0900

    commit

--- BAEKJOON/src/Main_14500_테트로미노.java
+++ BAEKJOON/src/Main_14500_테트로미노.java
@@ -1,38 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 회전을 테트로미노가 아니라 맵을 회전시키면 되나?
- */
 public class Main_14500_테트로미노 {
-	static class Tetromino {
-		int[] first;
-		int[] second;
-		int[] third;
-		int[] fourth;
-		
-		public Tetromino() {
-			// TODO Auto-generated constructor stub
-		}
-		
-		public Tetromino(int[] first, int[] second, int[] third, int[] fourth) {
-			this.first = first;
-			this.second = second;
-			this.third = third;
-			this.fourth = fourth;
-		}
-
-		@Override
-		public String toString() {
-			return "Tetromino [first=" + Arrays.toString(first) + ", second=" + Arrays.toString(second) + ", third="
-					+ Arrays.toString(third) + ", fourth=" + Arrays.toString(fourth) + "]";
-		}
-
-	}
-	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int N, M;
+	static int N, M, answer;
 	static int[][] map;
+	static boolean[][] isSelected;
 	
 	public static void init() {
 		map = new int[N][M];
@@ -52,11 +25,45 @@ public class Main_14500_테트로미노 {
 			}
 		}
 		
-		Tetromino tetro1 = new Tetromino();
-		Tetromino tetro2 = new Tetromino();
-		Tetromino tetro3 = new Tetromino();
-		Tetromino tetro4 = new Tetromino();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				isSelected = new boolean[N][M];
+				
+				isSelected[i][j] = true;
+				DFS(i, j, 1, map[i][j]);				
+			}
+		}
 		
+		System.out.println(answer);
 		
 	} // end of main
+
+	static final int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	
+	/**
+	 * @param point : 현지점
+	 * @param count : 선택된 칸의 개수
+	 */
+	private static void DFS(int r, int c, int count, int sum) {
+		if(count >= 4) {
+			answer = Math.max(answer, sum);
+			return;
+		}
+		
+		isSelected[r][c] = true;
+		
+		for(int d = 0; d < 4; d++) {
+			int nr = r + dir[d][0];
+			int nc = c + dir[d][1];
+			
+			if(nr >= 0 && nr < N && nc >= 0 && nc < M && !isSelected[nr][nc]) { // 범위 내에 있으면
+				isSelected[nr][nc] = true;
+				DFS(nr, nc, count+1, sum+map[nr][nc]);
+				
+				isSelected[nr][nc] = false;
+			}
+		}
+		
+	}
+	
 } // end of class
