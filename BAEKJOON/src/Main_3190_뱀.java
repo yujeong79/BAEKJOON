d630180660a488f03da5eaba173e8c7831898158
김유정 <aabbc0908>
2024-11-20 09:18:21 +0900

    commit

--- BAEKJOON/src/Main_3190_뱀.java
+++ BAEKJOON/src/Main_3190_뱀.java
@@ -6,20 +6,19 @@ import java.util.*;
  */
 public class Main_3190_뱀 {
 	static class Snake {
-		int r, c, d, length;
+		int r, c, d;
 
-		public Snake(int r, int c, int d, int length) {
+		public Snake(int r, int c, int d) {
 			this.r = r;
 			this.c = c;
 			this.d = d;
-			this.length = length;
 		}
 	}
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int N, K, L;
-	static boolean[][] board;
-	static Queue<int[]> dirInfo;
+	static boolean[][] board, isSnake;
+	static Queue<int[]> dirInfo, snakeBody;
 	static Snake snake;
 	
 	public static void main(String[] args) throws IOException {
@@ -44,8 +43,14 @@ public class Main_3190_뱀 {
 			if(D.equals("D")) dirInfo.add(new int[] {T, 1}); // 오른쪽으로 90도 회전
 			else dirInfo.add(new int[] {T, -1}); // 왼쪽으로 90도 회전
 		}
+		
+		dirInfo.add(new int[] {Integer.MAX_VALUE, 0});
 
-		snake = new Snake(0, 0, 1, 1);
+		snake = new Snake(0, 0, 1);
+		isSnake = new boolean[N][N];
+		isSnake[0][0] = true;
+		snakeBody = new LinkedList<>();
+		snakeBody.add(new int[] {0, 0});
 
 		int T = 0;
 		int[] command = dirInfo.poll();
@@ -56,6 +61,7 @@ public class Main_3190_뱀 {
 				if (T == command[0]) { // X초가 지났다면 뱀이 C 방향으로 회전
 					if(command[1] > 0) snake.d = (snake.d + command[1])%4;
 					else snake.d = (snake.d + 4 + command[1])%4;
+					
 					command = dirInfo.poll(); // 새로운 뱀의 방향 변환 정보로 갱신
 				}
 			} else {
@@ -74,22 +80,21 @@ public class Main_3190_뱀 {
 		snake.r += dir[snake.d][0];
 		snake.c += dir[snake.d][1];
 
-		if(snake.r >= 0 && snake.r < N && snake.c >= 0 && snake.c < N && !isBumped()) {
+		if(snake.r >= 0 && snake.r < N && snake.c >= 0 && snake.c < N && !isSnake[snake.r][snake.c]) {
+			isSnake[snake.r][snake.c] = true;
+			snakeBody.add(new int[] {snake.r, snake.c});
+			
 			if(board[snake.r][snake.c]) { // 사과가 있다면 사과를 먹고 몸길이 증가
 				board[snake.r][snake.c] = false;
-				snake.length++;
+			} else {
+				int[] tail = snakeBody.poll();
+				isSnake[tail[0]][tail[1]] = false;
 			}
 
 			return true;
 		}
 
 		// 벽을 만나거나 자신의 몸과 부딪힌 경우
-		else return false;
-	}
-
-	private static boolean isBumped() {
-
-
 		return false;
 	}
 
