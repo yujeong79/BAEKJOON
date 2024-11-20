ec490da90edfd8be58cc6b2d0a2671b07f87e6b3
김유정 <aabbc0908>
2024-11-20 09:40:11 +0900

    commit

---
+++ BAEKJOON/src/Main_3020_개똥벌레.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 4번째 구간은 밑에서부터 3번째칸과 4번째칸의 중간지점을 말한다.
+ */
+
+public class Main_3020_개똥벌레 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N, H;
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken()); // 동굴의 길이
+		H = Integer.parseInt(st.nextToken()); // 동굴의 높이
+		
+		
+		
+	} // end of main
+} // end of class
