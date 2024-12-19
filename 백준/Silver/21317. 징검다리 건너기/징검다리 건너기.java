9b18ba2407c9f3f6da9b611395e07d9d0deb814d
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2024-12-19 20:03:08 +0900

    [Silver I] Title: 징검다리 건너기, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
@@ -1,44 +1,33 @@
-import java.util.*;
-import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[][] board = new int[2][Math.max(4,N)];
-		for (int i = 1; i <= N-1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			board[0][i] = Integer.parseInt(st.nextToken());
-			board[1][i] = Integer.parseInt(st.nextToken());
-			
-		}
-//		for (int i = 0; i < board.length; i++) {
-//			System.out.println(Arrays.toString(board[i]));
-//		}
-		
-		int K = Integer.parseInt(br.readLine());
-		
-		int[][] dp = new int[2][Math.max(4, N+1)];
-		for (int i = 0; i < dp.length; i++) {
-			Arrays.fill(dp[i], 10000);
-		}
-		dp[0][0] = 0;
-		dp[0][1] = 0;
-		dp[1][0] = 0;
-		dp[1][1] = 0;
-		dp[0][2] = board[0][1];
-		dp[0][3] = Math.min(dp[0][2] + board[0][2], dp[0][0]+board[1][1]);
-		for (int i = 4; i <= N; i++) {
-			dp[0][i] = Math.min(dp[0][i-1] + board[0][i-1], dp[0][i-2] + board[1][i-2]);
-			dp[1][i] = Math.min(Math.min(dp[1][i-1] + board[0][i-1], dp[1][i-2] + board[1][i-2]),dp[0][i-3]+K);
-		}
-		int answer = Math.min(dp[0][N], dp[1][N]);
-		System.out.println(answer);
-//		
-//		for (int i = 0; i < dp.length; i++) {
-//			System.out.println(Arrays.toString(dp[i]));
-//		}
-		
-		
-	}
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n - 1; i++)
+            arr[i] = new int[] { read(), read() };
+        int k = read();
+        int[][] dp = new int[n + 3][2];
+        for (int i = 0; i < n + 3; i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        dp[0][0] = dp[0][1] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            dp[i + 1][0] = Math.min(dp[i + 1][0], dp[i][0] + arr[i][0]);
+            dp[i + 2][0] = Math.min(dp[i + 2][0], dp[i][0] + arr[i][1]);
+            dp[i + 3][1] = Math.min(dp[i + 3][1], dp[i][0] + k);
+            dp[i + 1][1] = Math.min(dp[i + 1][1], dp[i][1] + arr[i][0]);
+            dp[i + 2][1] = Math.min(dp[i + 2][1], dp[i][1] + arr[i][1]);
+        }
+        System.out.println(Math.min(dp[n - 1][0], dp[n - 1][1]));
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
 }
