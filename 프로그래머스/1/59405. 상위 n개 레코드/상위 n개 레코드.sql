32a97ee3d91531c7a8463a986345870400963f4a
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-06-12 11:15:41 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,5 +1,9 @@
 -- 동물 보호소에 가장 먼저 들어온 동물의 이름 조회
-SELECT NAME
-FROM ANIMAL_INS
-ORDER BY DATETIME
-LIMIT 1;
+SELECT name
+FROM (
+    SELECT
+        name,
+        RANK() OVER (ORDER BY datetime) AS rnk
+    FROM animal_ins
+) temp
+WHERE rnk = 1;
