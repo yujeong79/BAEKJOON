900e64866ec6e0c6fa98463ff590c60be7bfb782
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-06-17 15:23:52 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,4 +1,9 @@
--- 코드를 작성해주세요
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME FROM DEVELOPER_INFOS
-WHERE 'Python' IN (SKILL_1, SKILL_2, SKILL_3)
-ORDER BY ID ASC;
+-- 조회 : ID, 이메일, 이름, 성
+-- 조건 : skill_1, skill_2, skill_3 중 하나가 python
+-- 정렬 : ID 기준으로 오름차순
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 = 'Python' 
+    OR SKILL_2 = 'Python'
+    OR SKILL_3 = 'Python'
+ORDER BY ID;
