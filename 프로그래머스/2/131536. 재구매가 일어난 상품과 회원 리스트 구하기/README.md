c2875430509fe79ebc738063c1de041d910837d3
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-06-28 15:51:23 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/README.md
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 재구매가 일어난 상품과 회원 리스트 구하기 - 131536 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131536) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131536#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 03월 28일 09:49:36
+2025년 06월 28일 15:51:21
 
 ### 문제 설명
 
--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,4 +1,9 @@
-SELECT user_id, product_id FROM online_sale
-GROUP BY user_id, product_id
-HAVING COUNT(*) >= 2
-ORDER BY user_id ASC, product_id DESC;
+-- 조회 : 회원 ID, 상품 ID
+-- 그룹 : 회원 ID + 상품 ID
+-- 조건 : 재구매
+-- 정렬 : 회원 ID 기준, 상품 ID 기준 내림차순
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) > 1
+ORDER BY USER_ID, PRODUCT_ID DESC;
