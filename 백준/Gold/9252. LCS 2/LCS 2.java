14f7162ce32af13fe865afd3e9a2cc1d4f18327c
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-08-03 22:43:09 +0900

    [Gold IV] Title: LCS 2, Time: 136 ms, Memory: 19220 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {	
+    static char[] sentence1, sentence2;
+    static int[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	sentence1 = br.readLine().toCharArray();
+    	sentence2 = br.readLine().toCharArray();
+    	
+    	dp = new int[sentence1.length+1][sentence2.length+1];
+
+    	for(int i = 1; i <= sentence1.length; i++) {
+    		for(int j = 1; j <= sentence2.length; j++) {
+    			if(sentence1[i-1] == sentence2[j-1]) {
+    				dp[i][j] = dp[i-1][j-1]+1;
+    			} else {
+    				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+    			}
+    		}
+    	}
+    	
+    	Stack<Character> st = new Stack<>();
+    	int i = sentence1.length;
+    	int j = sentence2.length;
+    	
+    	while(i > 0 && j > 0) {
+    		if(dp[i][j] == dp[i-1][j]) {
+    			i--;
+    		} else if (dp[i][j] == dp[i][j-1]) {
+    			j--;
+    		} else {
+    			st.add(sentence1[i-1]);
+    			i--;
+    			j--;
+    		}
+    	}
+    	
+    	String answer = "";
+    	while(!st.isEmpty()) {
+    		answer += st.pop();
+    	}
+    	
+    	System.out.println(dp[sentence1.length][sentence2.length]);
+    	System.out.println(answer);
+    	
+    }
+}
