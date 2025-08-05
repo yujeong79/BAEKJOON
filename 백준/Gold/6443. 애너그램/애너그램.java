8f39a695cc7f0bfe01b8923b18f5bb0c3ae40467
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-08-05 21:05:25 +0900

    [Gold V] Title: 애너그램, Time: 760 ms, Memory: 59888 KB -BaekjoonHub

---
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+ 
+ 
+public class Main {
+ 
+    static int n;
+    static int[] check;
+    static Stack<Character> stack =new Stack<>();
+    static PriorityQueue<String> queue = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+ 
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            char[] chars = br.readLine().toCharArray();
+            int length = chars.length;
+            check = new int[26];
+            for(char now: chars){
+                check[now-'a']++;
+            }
+            dfs(chars,length);
+            while(!queue.isEmpty()){
+                sb.append(queue.poll()).append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+ 
+    private static void dfs(char [] s, int limit) {
+        if(limit== stack.size()){
+            StringBuilder sb = new StringBuilder();
+            for(char now: stack){
+                sb.append(now);
+            }
+            queue.add(sb.toString());
+        }
+ 
+        for(int i=0; i<26; i++){
+            if(check[i]>0){
+                check[i]--;
+                stack.push((char)(i+'a'));
+                dfs(s,limit);
+                stack.pop();
+                check[i]++;
+            }
+        }
+    }
+}
