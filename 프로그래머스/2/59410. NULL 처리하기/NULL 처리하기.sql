dc021f6df0f585f836986f91768ecc711ea6d336
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-08-19 17:03:15 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,5 +1,6 @@
--- 조회 : 생물 종, IFNULL(이름, 'No name'), 성별, 중성화 여부
--- 정렬 : 아이디 기준
-SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name') AS NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
+-- 조회 : 생물 종, 이름, 성별, 중성화 여부
+-- 이름 : 이름이 NULL이면 No name
+-- 정렬 : 아이디순
+select animal_type, ifnull(name, 'No name') as name, sex_upon_intake
+from animal_ins
+order by animal_id;
