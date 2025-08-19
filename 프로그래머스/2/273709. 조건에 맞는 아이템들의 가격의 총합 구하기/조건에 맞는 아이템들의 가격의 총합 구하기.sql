ba1836237158a6d61fa0e8b982742da9f2b2ce5e
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-08-19 17:11:25 +0900

    [level 2] Title: 조건에 맞는 아이템들의 가격의 총합 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
+++ 프로그래머스/2/273709. 조건에 맞는 아이템들의 가격의 총합 구하기/조건에 맞는 아이템들의 가격의 총합 구하기.sql
@@ -1,5 +1,5 @@
 -- 조회 : 가격의 총합 AS TOTAL_PRICE
--- 조건 : 희귀도 = LEGEND
-SELECT SUM(PRICE) AS TOTAL_PRICE
-FROM ITEM_INFO
-WHERE RARITY = 'LEGEND';
+-- 조건 : 희귀도가 LEGEND
+select sum(price) as total_price
+from item_info
+where rarity = 'LEGEND';
