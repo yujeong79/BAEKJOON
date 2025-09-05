72dd59e5af38059352f612ed0515318995654b95
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-05 18:49:52 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,8 @@
--- 조회 : 아이디, 이름, 중성화 여부
--- 조건 : 이름이 lucy, ella, pickle, rogan, sabrina, mitty
+-- 조회 : 동물의 아이디, 이름, 성별 및 중성화 여부
+-- 조건 : 이름이 lucy, ella, pickel, rogan, sabrina, mitty
+-- 정렬 : 아이디 기준
+
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
