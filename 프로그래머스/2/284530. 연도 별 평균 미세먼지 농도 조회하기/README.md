ae86f91e681762cf12caf6a1273fea44bad8567c
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-05 18:45:58 +0900

    [level 2] Title: 연도 별 평균 미세먼지 농도 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/README.md
+++ 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 연도 별 평균 미세먼지 농도 조회하기 - 284530 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/284530#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/284530) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 18:44:44
+2025년 09월 05일 18:45:56
 
 ### 문제 설명
 
--- 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
+++ 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -1,10 +1,13 @@
--- 조회 : 연도별 평균 미세먼지 오염도 AS PM10, 평균 초미세먼지 오염도 AS PM2.5
---          => 둘 다 셋째 자리에서 반올림
--- 그룹 : 연도
+-- 조회 : 연도, 평균 미세먼지 오염도 AS PM10, 평균 초미세먼지 오염도 PM2.5
+-- PM10, PM2.5 : 소수 셋째 자리에서 반올림
 -- 조건 : LOCATION2 = 수원
--- 정렬 : 연도 기준
-SELECT YEAR(YM) AS YEAR, ROUND(AVG(PM_VAL1), 2) AS 'PM10', ROUND(AVG(PM_VAL2), 2) AS 'PM2.5'
+-- 그룹 : 연도
+-- 결과 : 연도 기준
+
+SELECT YEAR(YM) AS 'YEAR', 
+    ROUND(AVG(PM_VAL1), 2) AS 'PM10', 
+    ROUND(AVG(PM_VAL2), 2) AS 'PM2.5'
 FROM AIR_POLLUTION
 WHERE LOCATION2 = '수원'
-GROUP BY YEAR(YM)
-ORDER BY YEAR(YM);
+GROUP BY YEAR
+ORDER BY YEAR;
