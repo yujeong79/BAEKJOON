5383537413d697dcbbb518d3c14654bf66276e12
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-10 18:21:27 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,10 +1,10 @@
--- 조회 : 다음 업그레이드 아이템의 아이템ID, 아이템명, 아이템의 희귀도
--- 조건 : 아이템의 희귀도가 'RARE'인 아이템
+-- 조회 : 아이템ID, 아이템명, 희귀도
+-- 조건 : 희귀도가 'RARE'인 아이템들의 모든 다음 업그레이드 아이템
 -- 정렬 : 아이템ID 기준 내림차순
-SELECT ITEM_ID, ITEM_NAME, RARITY
-FROM ITEM_INFO
-WHERE ITEM_ID IN (SELECT t.ITEM_ID FROM ITEM_INFO i LEFT JOIN ITEM_TREE t ON i.ITEM_ID = t.PARENT_ITEM_ID
-                  WHERE i.RARITY = 'RARE' AND t.ITEM_ID IS NOT NULL
-                 )
-ORDER BY ITEM_ID DESC;
 
+SELECT I.ITEM_ID, ITEM_NAME, RARITY
+FROM ITEM_INFO I LEFT JOIN ITEM_TREE T ON I.ITEM_ID = T.ITEM_ID
+WHERE PARENT_ITEM_ID IN (SELECT ITEM_ID
+                        FROM ITEM_INFO
+                        WHERE RARITY = 'RARE')
+ORDER BY ITEM_ID DESC;
