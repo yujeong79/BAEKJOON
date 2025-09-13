1f74f2b3b61ee18f71dec14d503400ee2b3b3211
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-13 17:25:10 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,7 +1,7 @@
--- 조회 : 아이디, 이름
+-- 조회 : 동물ID, 이름
 -- 정렬 : 보호 기간 기준 내림차순
--- LIMIT : 2
-select o.animal_id, o.name
-from animal_outs o left join animal_ins i on o.animal_id = i.animal_id
-order by datediff(o.datetime, i.datetime) desc
-limit 2;
+-- 리밋 : 2
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+ORDER BY DATEDIFF(O.DATETIME, I.DATETIME) DESC
+LIMIT 2;
