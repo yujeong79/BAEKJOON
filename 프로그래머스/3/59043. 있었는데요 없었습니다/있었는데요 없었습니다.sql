e0816cdcae4fb43c5428061a7e30ba8b15865d93
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-13 17:19:47 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,7 @@
--- 조회 : 아이디, 이름
+-- 조회 : 동물ID, 이름
 -- 조건 : 보호 시작일보다 입양일이 더 빠른 동물
--- 정렬 : 보호시작일 기준
-select o.animal_id, o.name
-from animal_outs o left join animal_ins i on o.animal_id = i.animal_id
-where o.datetime < i.datetime
-order by i.datetime;
+-- 정렬 : 보호 시작일 순
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.DATETIME < I.DATETIME
+ORDER BY I.DATETIME;
