8512ee881d8100ac915bb126d7d432d32f27c237
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-14 20:14:13 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 07월 03일 13:57:09
+2025년 09월 14일 20:14:10
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,13 +1,14 @@
--- 조회 : 사용자 ID, 닉네임, 전체주소, 전화번호
--- 전체주소 : CITY + ADDRESS1 + ADDRESS2
--- 전화번호 : - 삽입
--- 조건 : 중고 거래 게시물을 3건 이상 등록한 사용자
+-- 조회 : 사용자ID, 닉네임, 전체주소, 전화번호
+-- 전제 주소 : 시 + 도로명 주소 + 상세 주소
+-- 전화번호 : xxx-xxxx-xxxx
+-- 조건 : 게시물을 3건 이상 등록한 사용자
 -- 정렬 : 회원ID 기준 내림차순
-SELECT U.USER_ID, 
-    NICKNAME, 
-    CONCAT(CITY, " ", STREET_ADDRESS1, " ", STREET_ADDRESS2) AS '전체주소', 
-    CONCAT(LEFT(TLNO, 3), "-", SUBSTRING(TLNO, 4, 4), "-", RIGHT(TLNO, 4)) AS '전화번호'
-FROM USED_GOODS_BOARD B LEFT JOIN USED_GOODS_USER U ON B.WRITER_ID = U.USER_ID
-GROUP BY B.WRITER_ID
-HAVING COUNT(BOARD_ID) >= 3
-ORDER BY U.USER_ID DESC;
+
+SELECT USER_ID,
+    NICKNAME,
+    CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소',
+    CONCAT(LEFT(TLNO, 3), '-', SUBSTRING(TLNO, 4, 4), '-', RIGHT(TLNO, 4)) AS '전화번호'    
+FROM USED_GOODS_BOARD D JOIN USED_GOODS_USER U ON D.WRITER_ID = U.USER_ID
+GROUP BY USER_ID
+HAVING COUNT(*) >= 3
+ORDER BY USER_ID DESC;
