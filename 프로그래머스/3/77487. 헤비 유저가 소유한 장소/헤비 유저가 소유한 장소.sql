18c15f640b505e9427ed3a7991ddc6133dced03a
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-19 18:01:43 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,12 +1,12 @@
--- 조회 : ID, NAME, HOST_ID
--- 그룹 : HOST_ID
--- HAVING : 공간을 둘 이상 등록한 사용자
--- 정렬 : ID 기준
+-- 조회: ID, NAME, HOST_ID
+-- 그룹: HOST_ID
+-- 해빙: 개수가 2 이상
+-- 정렬: ID 기준
+
 SELECT ID, NAME, HOST_ID
 FROM PLACES
-WHERE HOST_ID IN (SELECT HOST_ID 
+WHERE HOST_ID IN (SELECT HOST_ID
                     FROM PLACES
                     GROUP BY HOST_ID
-                    HAVING COUNT(*) >= 2
-                 )
+                    HAVING COUNT(*) >= 2)
 ORDER BY ID;
