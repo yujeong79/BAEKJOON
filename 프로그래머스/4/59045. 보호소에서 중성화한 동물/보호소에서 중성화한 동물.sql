bd0294246a3cce75cde5feee5b9e49f3f7cc14b1
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-25 11:43:46 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,9 @@
--- 조회 : 아이디, 생물 종, 이름
--- 조건 : 보호소에 들어올 당시엔 중성화되지 않았지만(%Intact%) 보호소를 나갈 당시엔 중성화(Neutred/Spayed)된 동물
--- 정렬 : 아이디 기준
-SELECT o.animal_id, o.animal_type, o.name
-FROM ANIMAL_OUTS o LEFT JOIN ANIMAL_INS i ON o.animal_id = i.animal_id
-WHERE (o.sex_upon_outcome LIKE '%Neutered%' OR o.sex_upon_outcome LIKE '%Spayed%')
-    AND i.sex_upon_intake LIKE '%Intact%'
-ORDER BY o.animal_id;
+-- 조회 : ANIMAL_ID, ANIMAL_TYPE, NAME
+-- 조건 : 보호소에 들어올 당시에는 중성화되지 않았지만 나갈 당시에는 중성화된 동물
+-- 정렬 : ANIMAL_ID 기준
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE LIKE 'Intact%'
+    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%' OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY ANIMAL_ID;
