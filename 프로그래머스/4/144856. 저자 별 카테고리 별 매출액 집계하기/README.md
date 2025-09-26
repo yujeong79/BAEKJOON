52bf16e5b83a612672ac5b56b44a6c6f5b9d96e0
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-26 18:52:11 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 저자 별 카테고리 별 매출액 집계하기 - 144856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 07월 05일 09:23:52
+2025년 09월 26일 18:52:09
 
 ### 문제 설명
 
--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,11 +1,11 @@
--- 조회 : 저자 ID, 저자명, 카테고리, 매출액
--- 조건 : 2022년 1월의 도서 판매 데이터
--- 그룹 : 저자, 카테고리
--- 정렬 : 저자 ID 기준, 카테고리 기준 내림차순
-SELECT b.author_id, a.author_name, b.category, sum(s.sales*b.price) AS TOTAL_SALES
-FROM book b 
-    JOIN author a ON b.author_id = a.author_id
-    JOIN book_sales s ON b.book_id = s.book_id
-WHERE DATE_FORMAT(s.sales_date, '%Y%m') = '202201'
-GROUP BY b.author_id, b.category
-ORDER BY b.author_id, b.category DESC;
+-- 조회 : 저자ID, 저자명, 카테고리, 매출액
+-- 그룹 : 저자별, 카테고리별
+-- 정렬 : 저자ID 기준, 카테고리 기준 내림차순
+
+SELECT A.AUTHOR_ID, A.AUTHOR_NAME, B.CATEGORY, SUM(S.SALES * B.PRICE) AS TOTAL_SALES
+FROM BOOK_SALES S 
+    LEFT JOIN BOOK B ON S.BOOK_ID = B.BOOK_ID
+    LEFT JOIN AUTHOR A ON B.AUTHOR_ID = A.AUTHOR_ID
+WHERE SALES_DATE BETWEEN '2022-01-01' AND '2022-01-31'
+GROUP BY A.AUTHOR_ID, B.CATEGORY
+ORDER BY A.AUTHOR_ID, B.CATEGORY DESC;
