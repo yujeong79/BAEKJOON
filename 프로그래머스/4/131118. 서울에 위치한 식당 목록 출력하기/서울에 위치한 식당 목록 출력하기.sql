74f56961311ba2b3e60beb352c352f79200656cf
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-09-28 13:48:03 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,14 +1,15 @@
 -- 조회 : 식당ID, 식당이름, 음식종류, 즐겨찾기수, 주소, 리뷰평균점수
--- 리뷰평균점수 : 소수점 세 번째 자리에서 반올림
--- 조건 : 서울에 위치한 식당들
--- 정렬 : 평균점수 기준 내림차순, 즐겨찾기 기준 내림차순
+-- 조건 : 서울에 위치
+-- 리뷰평균점수 : 소수점 세번째자리에서 반올림
+-- 정렬 : 평균점수 기준 내림차순, 즐겨찾기수 기준 내림차순
+
 SELECT I.REST_ID, 
-    REST_NAME, 
-    FOOD_TYPE, 
-    FAVORITES, 
-    ADDRESS, 
+    REST_NAME,
+    FOOD_TYPE,
+    FAVORITES,
+    ADDRESS,
     ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
-FROM REST_INFO I JOIN REST_REVIEW R ON I.REST_ID = R.REST_ID
+FROM REST_REVIEW R LEFT JOIN REST_INFO I ON R.REST_ID = I.REST_ID
+WHERE ADDRESS REGEXP '^서울'
 GROUP BY I.REST_ID
-HAVING ADDRESS LIKE '서울%'
 ORDER BY SCORE DESC, FAVORITES DESC;
