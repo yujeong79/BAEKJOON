2be1c78afeae619e415ecec9b9ad3ec3ada878cf
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2025-10-09 16:17:02 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,12 +1,18 @@
--- 조회: 첨부파일 경로
--- 첨부파일 경로: /home/grep/src/BOARD_ID/FILD_IDFILENAMEFILE_EXT
--- 조건: 조회수가 가장 높은 중고거래 게시물
--- 정렬: FILE_ID 기준 내림차순
+-- 조회 : FILE_PATH
+-- FILE_PATH : /home/grep/src/BOARD_ID/FILE_IDFILE_NAMEFILE_EXT
+-- 조건 : 조회수가 가장 높은 중고거래 게시물
+-- 정렬 : FILD_ID 기준 내림차순
+
+WITH CTE1 AS (
+    SELECT BOARD_ID,
+        RANK() OVER(ORDER BY VIEWS DESC) AS RNK
+    FROM USED_GOODS_BOARD
+), CTE2 AS (
+    SELECT FILE_ID, FILE_EXT, FILE_NAME, F.BOARD_ID
+    FROM USED_GOODS_FILE F LEFT JOIN CTE1 C1 ON F.BOARD_ID = C1.BOARD_ID
+    WHERE RNK = 1
+)
 
 SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
-FROM USED_GOODS_FILE
-WHERE BOARD_ID = (SELECT BOARD_ID
-                    FROM USED_GOODS_BOARD
-                    ORDER BY VIEWS DESC
-                    LIMIT 1)
+FROM CTE2
 ORDER BY FILE_ID DESC;
